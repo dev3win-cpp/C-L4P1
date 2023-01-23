f667a0fbfcc4254121b52fcc4dcd572c534f257b
Tadeusz <tadeusz.potoniec@parhelium.com>
2023-01-23 13:02:28 +0100

    In Lesson 4 I used a near-random generation of 6 numbers in the range 49. In Lesson 5 I use an array.

--- C++L4P1/main.cpp
+++ C++L4P1/main.cpp
@@ -3,12 +3,24 @@
 //  C++L4P1
 //
 //  Created by Tadeusz Potoniec on 23/01/2023.
-//
+//  Based on https://www.youtube.com/watch?v=ErOzmh3BiXU&list=PLOYHgt8dIdoxx0Y5wzs7CFpmBzb40PaDo
 
 #include <iostream>
+#include <unistd.h> //The library used for countdown time
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int number;
+
+int main() {
+    cout<<"Welcome to the draw! the lock will be released in 3 seconds";
+    sleep(3);
+    cout<<endl;
+    srand((int) time(0));
+    for(int i=1;i<=6;i++)
+    {
+        number=rand()%49+1;
+        sleep(1);
+        cout<<number<<"\a"<<endl;
+    }
     return 0;
 }
